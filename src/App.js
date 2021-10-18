3fbbf9fdf0cb10bf74386679271beb54c3daf190
abhi08999 <abhipatidar17081999@gmail.com>
2021-10-18 13:32:33 +0530

    gh-pages deploy

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 // Home page
 import Home from "./Pages/home";
@@ -7,13 +8,18 @@ import Home from "./Pages/home";
 function App() {
   return (
     <>
+          <HashRouter basename="/">
+
       <Switch>
       <Route exact path="/" component={Home}/>
             
         <Route path="*" render={() => <Redirect to="/" />} />
 
       </Switch>
+      </HashRouter>
+
     </>
+    
   );
 }
 
